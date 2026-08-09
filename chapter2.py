21cf63029f4c1b5a5fc5a0ba0739afec4ac7dd81
jingtian <jwu.blue.sky@gmail.com>
2026-08-09 16:06:31 -0700

    chapter 2

---
+++ chapter2.py
@@ -0,0 +1,20 @@
+message = "Hello Python World"
+print(message)
+
+message = "Hello Python Crash Course World"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+# Say hello to everyone.
+print("Hello Python people!")
