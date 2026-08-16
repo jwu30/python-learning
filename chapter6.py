0fa2ffb6c278461da721fa15a059fc57e81f089f
jingtian <jwu.blue.sky@gmail.com>
2026-08-15 19:14:43 -0700

    chapter 7 dictionary

---
+++ chapter6.py
@@ -0,0 +1,4 @@
+name = {'0':"Jack"}
+print("My name " +
+      "is "+ 
+      name['0'])
---
+++ chapter7.py
@@ -0,0 +1,5 @@
+user = {"jack":"python","tom":"C","jerry":"Java"}
+
+for k,v in user.items():
+	print("\nKey: " + k)
+	print("Value: " + v)
