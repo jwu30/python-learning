e9173314b132325ef9c0511f56ded3353249056a
jingtian <jwu.blue.sky@gmail.com>
2026-08-19 15:07:21 -0700

    chapter 8 part2

--- chapter8.py
+++ chapter8.py
@@ -7,3 +7,15 @@ fun(5,6)
 a = ''
 if not a:
 	print(3)
+
+def fun1(*b):
+	for i in b:
+		print(i)
+
+fun1('a','b','c')
+
+def fun2(**b):
+	for i in b.values():
+		print(i)
+
+fun2(a='d',b='e',c='f')
